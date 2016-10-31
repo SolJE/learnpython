e2dfc37c2bed45dfa91da4a2c3478c1625192e25
Sol Je <je-eee@163.com>
2016-10-31 10:01:52 +0800

    装饰器执行顺序

--- lxfpy/python_装饰器.py
+++ lxfpy/python_装饰器.py
@@ -1,15 +1,23 @@
 def f1(arg):
-    print ("f1")
-    rl = arg()
-    print(rl)
+    print ("f1") #实际打印“f1”
+    rl = arg() #此句打印f2
+    print(rl) #实际打印"f2r"
     return rl + "f1"
     
 @f1
 def f2(arg = ""):
-    print ("f2")
+    print ("f2") #实际打印“f2”
     return arg + "f2r"
 
-print ("start")
-print (f2)
+print ("start") #实际打印“start”
+print (f2) #实际打印“f2rf1”
 #print f2("1") 出错
 #print f1(None)
+
+#实际执行和打印顺序为
+#第2行：f1
+#第3行：f2
+#第4行：f2r
+#第12行：start
+#第13行：f2rf1
+#在def f2前@f1 执行的是f2=f1(f2())
