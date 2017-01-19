5f48fc9ed5acc35d388e14aff33f98168bd15bdf
Sol Je <je-eee@163.com>
2017-01-19 15:45:54 +0800

    telnet

--- exercise/python_telnetlib.py
+++ exercise/python_telnetlib.py
@@ -1,38 +1,25 @@
-import getpass
 import telnetlib
-import re
-import time
 
-HOST ="192.168.169.1"
-user = "root"
-password = "admin"
-tn = telnetlib.Telnet(HOST)
-tn.set_debuglevel(5)
+HOST = "192.168.169.1"
+USER = "root"
+PASSWORD = "hebao"
+TN = telnetlib.Telnet(HOST)
+TN.set_debuglevel(5)
 #mac = input("Input Mac Adress:")
 try:
-    tn.read_until(b"login: ")
-    tn.write(user.encode('ascii') + b"\n")
-    #if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
-    #time.sleep(2)
-    if tn.read_until(b"BusyBox",10):
-        print("LOGGING EOFError")
-        tn.close()
-    #mac_suffix = mac[-6:]
-    #mac_lan = hex((int(mac_suffix,base=16)+2))
-    #mac_wan = hex((int(mac_suffix,base=16)+3))
-
-
-    #mac_two = re.findall(r'.{2}',mac)#mac_two是一个List
-
-    else:
-        tn.write(b"ls\n")
-        tn.write(b"ps\n")
-        #tn.write(b"iwpriv ra0 e2p 4=9684\n")
-        tn.write(b"exit\n")
-        tn.read_all()
-        print("YES")
-        #print(tn.read_all().decode('ascii'))
+    TN.read_until(b"login: ")
+    TN.write(USER.encode('ascii') + b"\n")
+    if PASSWORD:
+        TN.read_until(b"Password: ")
+        TN.write(PASSWORD.encode('ascii') + b"\n")
+    #if TN.read_some().index("BusyBox") >= 0:
+    print("TEST")
+    #print(TN.read_some())
+    print("TEST")
+    TN.write(b"ls\n")
+    TN.write(b"ps\n")
+    TN.write(b"exit\n")
+    print("YES")
+    print(TN.read_all().decode('ascii'))
 except Exception as e:
     print("NO")
